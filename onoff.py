9f8aa9913cd316a15f1b275856799072849bb62b
Martin FOREST <97240787+mforest01@users.noreply.github.com>
2022-02-14 15:47:08 +0100

    Update onoff.py

--- onoff.py
+++ onoff.py
@@ -14,7 +14,7 @@ pj.power.SetWakeUpOnCharge(0)
 # Make sure power to the Raspberry Pi is stopped to not deplete
 # the battery
 pj.power.SetSystemPowerSwitch(0)
-pj.power.SetPowerOff(30)
+pj.power.SetPowerOff(5)
 # Now turn off the system
 os.system("sudo shutdown -h now")
 
