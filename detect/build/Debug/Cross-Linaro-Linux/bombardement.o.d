054c973d9a9e384e7a3c7c66669ae07768e97498
Martin FOREST <97240787+mforest01@users.noreply.github.com>
2022-03-10 14:40:23 +0100

    Add files via upload

---
+++ detect/build/Debug/Cross-Linaro-Linux/bombardement.o.d
@@ -0,0 +1,10 @@
+build/Debug/Cross-Linaro-Linux/bombardement.o: bombardement.cpp \
+ bombardement.h \
+ /home/snir2g1/NetBeansProjects/cppGPIO/include/cppgpio/buttons.hpp \
+ /home/snir2g1/NetBeansProjects/cppGPIO/include/cppgpio/gpio.hpp
+
+bombardement.h:
+
+/home/snir2g1/NetBeansProjects/cppGPIO/include/cppgpio/buttons.hpp:
+
+/home/snir2g1/NetBeansProjects/cppGPIO/include/cppgpio/gpio.hpp:
---
+++ detect/build/Debug/Cross-Linaro-Linux/main.o.d
@@ -0,0 +1,9 @@
+build/Debug/Cross-Linaro-Linux/main.o: main.cpp bombardement.h \
+ /home/snir2g1/NetBeansProjects/cppGPIO/include/cppgpio/buttons.hpp \
+ /home/snir2g1/NetBeansProjects/cppGPIO/include/cppgpio/gpio.hpp
+
+bombardement.h:
+
+/home/snir2g1/NetBeansProjects/cppGPIO/include/cppgpio/buttons.hpp:
+
+/home/snir2g1/NetBeansProjects/cppGPIO/include/cppgpio/gpio.hpp:
