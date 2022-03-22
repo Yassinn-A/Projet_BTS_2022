5742261d1f7b4f3af6d036fbe90e6c6924e3ede5
Martin FOREST <97240787+mforest01@users.noreply.github.com>
2022-03-22 15:16:08 +0100

    Update Reboot.cpp

--- initCube2022-maj/src/Reboot.cpp
+++ initCube2022-maj/src/Reboot.cpp
@@ -11,6 +11,15 @@
  * Created on 23 juin 2020, 17:41
  */
 #include "../defs/Reboot.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <iostream>
+#include <math.h>
+#include <sys/ioctl.h>
+using namespace std;
 
 Reboot::Reboot() {
     dateHour = "0000/00/00 00:00:00";
@@ -51,3 +60,61 @@ void Reboot::resetStatus(){
     number = 0;
 }
 
+void Reboot::rebootSolaire(){
+	unsigned char buf[4];
+        int fd;
+        ssize_t fd2;
+        fd = open("/dev/i2c-1",O_RDWR);
+        int addr = 0x14;        
+        ioctl(fd, I2C_SLAVE, addr);
+        
+        
+        //Ligne 1 : trame 1
+        buf[0] =0xC2 ;
+        if (write(fd, buf, 1) != 1) {
+        }
+        fd2 = read(fd,buf,3);
+        //Ligne 1: trame 2
+        buf[0]=0xC2;
+        buf[1]=0x25;
+        buf[2]=0x01;
+        buf[3]=0xDB;
+        if (write(fd, buf, 4) !=4){
+	}	
+	//Ligne 1 : trame 3
+	buf[0]=0xC2;
+	if (write(fd, buf, 1) != 1) {
+        }
+        fd2 = read(fd,buf,3);
+        
+        
+        //Ligne 2: trame 1
+        buf[0]=0x63;
+        buf[1]=0x00;
+        buf[2]=0xFF;
+        if (write(fd, buf, 3) !=3){
+	}
+	//Ligne 2 : trame 2
+	buf[0] =0x63 ;
+        if (write(fd, buf, 1) != 1) {
+        }
+        fd2 = read(fd,buf,2);
+        
+        
+        //Ligne 3
+        buf[0]=0x64;
+        buf[1]=0x00;
+        buf[2]=0xFF;
+        if (write(fd, buf, 3) !=3){
+	}
+        
+        
+	//Ligne 4
+	buf[0]=0x62;
+        buf[1]=0x05;
+        buf[2]=0xFA;
+        if (write(fd, buf, 3) !=3){
+	}
+	//Ligne pour quitter le terminal
+	system("sudo shutdown -h now");
+}
