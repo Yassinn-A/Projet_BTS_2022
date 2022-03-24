f7384ca1182ec41584d9ed6492792603afc37203
Martin FOREST <97240787+mforest01@users.noreply.github.com>
2022-03-24 14:48:47 +0100

    Add files via upload

---
+++ initCube2022-maj/defs/PowerControler.h
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   PowerControler.h
+ * Author: snir2g1
+ *
+ * Created on 22 mars 2022, 16:14
+ */
+
+#ifndef POWERCONTROLER_H
+#define POWERCONTROLER_H
+#include "../include/cppgpio/buttons.hpp"
+#include "./I2C.h"
+
+class PowerControler :public GPIO::InputDetect,public I2C{
+public:
+    PowerControler();
+    virtual ~PowerControler();
+    void triggered(unsigned int gpio);
+    void securedReboot();
+private:
+
+};
+
+#endif /* POWERCONTROLER_H */
+
