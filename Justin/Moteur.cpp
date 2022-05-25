61110b10bf485c4bdcd62a6edc7108c202942113
Ju317 <98109924+Ju317@users.noreply.github.com>
2022-05-25 15:36:10 +0200

    Update Moteur.cpp

--- Justin/Moteur.cpp
+++ Justin/Moteur.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Moteur.h"
+#include "../defs/Moteur.h"
 
 using namespace std;
 using namespace GPIO;
@@ -29,56 +29,6 @@ void Moteur::nveille(){
 
 void Moteur::eteindre(){
     speed->off();
-}
-
-void Moteur::orientation(){
-    int luminosite1 = soleil->luminosity1(); //récupère valeur capteur 1
-    int luminosite2 = soleil->luminosity2();//récupère valeur capteur 2
-    if(luminosite1!=luminosite2+20 ||luminosite1!=luminosite2-20 ){ 
-        int lum1 = soleil->luminosity1(); //lum1 = valeur capteur 1
-        int lum2 = soleil->luminosity2();//lum2 = valeur capteur 2
-        if(lum1>lum2+20 || lum1>lum2-20){//si capteur 1 > +ou- 20 capteur2
-            lum1 = soleil->luminosity1();//lum1 change
-            lum2 = soleil->luminosity2();//lum2 change
-            tournerG();
-            while((lum1>lum2+20) ||(lum1<lum2-20) ){
-                cout<< "ACapteur 1: "<< lum1<<endl;
-                cout<< "ACapteur 2: "<<lum2<<endl;
-                lum1 = soleil->luminosity1(); //lum1 = valeur capteur 1
-                usleep(10000);
-                lum2 = soleil->luminosity2();//lum2 = valeur capteur 2
-                usleep(10000);
-                cout<< "PCapteur 1: " << lum1<<endl;
-                cout<< "PCapteur 2: "<< lum2<<endl;
-            }
-            tournerD();
-        }
-        if(lum2>lum1+20 || lum2>lum1-20){
-            lum1 = soleil->luminosity1();
-            lum2 = soleil->luminosity2();
-            tournerD();
-            while((lum2>lum1+20) ||(lum2<lum1-20) ){
-                cout<< "ACapteur 3: "<< lum1<<endl;
-                cout<< "ACapteur 4: "<<lum2<<endl;
-                lum1 = soleil->luminosity1();
-                usleep(10000);
-                lum2 = soleil->luminosity2();
-                usleep(10000);
-                cout<< "PCapteur 3: "<< lum1<<endl;
-                cout<< "PCapteur 4: "<<lum2<<endl;
-            }
-            tournerG();
-        }
-
-        luminosite1 = soleil->luminosity1();
-        luminosite2 = soleil->luminosity2();
-    }
-}
-
-thread Moteur::torientation(){
-    return thread([this]{
-        orientation();
-    });
 }
 
 Moteur::~Moteur() {
