4bc2b904335c48f3f403dbc96b55a2aea06f9203
Ju317 <98109924+Ju317@users.noreply.github.com>
2022-05-30 15:24:45 +0200

    Update Moteur.h

--- Justin/Moteur.h
+++ Justin/Moteur.h
@@ -1,10 +1,8 @@
 #ifndef MOTEUR_H
 #define MOTEUR_H
 #include "../include/cppgpio/output.hpp"
-#include "../defs/Soleil.h"
 
 using namespace GPIO;
-class Soleil;
 class Moteur {
 public:
     Moteur();
@@ -18,8 +16,6 @@ private:
     DigitalOut* direction = new DigitalOut(19);
     DigitalOut* speed = new DigitalOut(18);
     DigitalOut* sleep = new DigitalOut(26);
-    Soleil* soleil;
 };
 
-#endif /* MOTEUR_H */
 
