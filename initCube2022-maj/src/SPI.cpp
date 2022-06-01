37f445611285f3369dcbc0b2b4326d20b94c4cbd
Ju317 <98109924+Ju317@users.noreply.github.com>
2022-06-01 11:47:14 +0200

    Update SPI.cpp

--- initCube2022-maj/src/SPI.cpp
+++ initCube2022-maj/src/SPI.cpp
@@ -9,10 +9,10 @@
  * Author: linux
  * 
  * Created on 25 mars 2020, 23:05
+ * Modified on avril 2022
  */
 
 #include "../defs/SPI.h"
-#include "../defs/CameraPhoto.h"
 
 SPI::SPI() {
    //spi_cs0_fd = open("/dev/spidev0.0", O_RDWR);
@@ -31,16 +31,10 @@ int SPI::ouvrirPortSpi(int spi_device)
     spi_device = 0;
     spi_mode = SPI_MODE_1;
     spi_bitsPerWord = 8;
-    spi_speed = 8000000;		
+    spi_speed = 4000000;		
     spi_cs_fd = &spi_cs0_fd;
     //*spi_cs_fd = open("/dev/spidev0.0", O_RDWR);
 	spi_cs0_fd = open("/dev/spidev0.0", O_RDWR);;
-		std::cout<<"open spi_cs_fd = " << *spi_cs_fd <<endl;
-     if (spi_cs0_fd < 0)
-    {
-        perror("Error - Could not open SPI device");
-        exit(1);
-    }
     return spi_cs0_fd;
 }
 
@@ -51,11 +45,6 @@ int SPI::fermerPortSpi(int spi_device)
     int *spi_cs_fd;
     spi_cs_fd = &spi_cs0_fd;
     status_value = close(*spi_cs_fd);
-    if(status_value < 0)
-    {
-    	perror("Error - Could not close SPI device");
-    	exit(1);
-    }
     return(status_value);
 }
 
@@ -67,7 +56,6 @@ int SPI::ecrireSpi(int spi_device, uint8_t CMD[2],int Length)
     int retVal = -1;
     int spi_cs_fd;
     spi_cs_fd = spi_cs0_fd;
-	std::cout<<" ecrire spi_cs_fd = " << spi_cs_fd <<endl;
 	memset(&spi, 0, sizeof(spi));
     spi.tx_buf = (unsigned long)CMD;		
 	spi.rx_buf = (unsigned) NULL;
@@ -76,13 +64,7 @@ int SPI::ecrireSpi(int spi_device, uint8_t CMD[2],int Length)
     spi.speed_hz = spi_speed;
     spi.bits_per_word = spi_bitsPerWord;
     spi.cs_change = 0;		
-	std::cout<<" apr�s config spi_cs_fd = " << spi_cs_fd<<" Length" << Length <<endl;
     retVal = ioctl(spi_cs_fd, SPI_IOC_MESSAGE(1), &spi);
-    if(retVal < 0)
-    {
-	perror("Error - Problem transmitting spi data..ioctl W");
-	exit(1);
-    }
     return retVal;
 }
 
@@ -111,60 +93,5 @@ int SPI::lireSpi(int spi_device,uint8_t CMD[1],uint8_t FLAG[1],int Length)
     spi[1].bits_per_word = spi_bitsPerWord;
     spi[1].cs_change = 0;
     retVal = ioctl(spi_cs_fd, SPI_IOC_MESSAGE(2), &spi);
-    if(retVal < 0)
-    {
-		perror("Error - Problem transmitting spi data.. r1 ioctl ");
-		exit(1);
-    }
     return retVal;
-}
-
-void SPI::verifSpi()
-{
-   uint8_t CMD[1];
-   uint8_t FLAG[1];
-       FLAG[0]=0;
-    CMD[0]=ARDUCHIP_TRIG;
-   do {
-
-    //ecrireSpi(0,CMD,1);
-    lireSpi(0,CMD, FLAG,1);
-	usleep(10000);
-    }
-    while ((FLAG[0]  & 0x8) != 0x8);
-}
-
-void SPI::RecuperationIMG() { //Doit aller dans CameraPhoto
-	uint8_t CMD[1];
-	uint8_t FLAG[1];
-	uint8_t image[JPEG_BUF_SIZE];
-
-	unsigned int len1, len2, len3, count = 0;
-
-	CMD[0]=FIFO_SIZE1;
-	lireSpi(0,CMD, FLAG,1);
-	len1 = FLAG[0];
-
-	CMD[0]=FIFO_SIZE2;
-	lireSpi(0,CMD, FLAG,1);
-	len2 = FLAG[0];
-
-	CMD[0]=FIFO_SIZE3;
-	lireSpi(0,CMD, FLAG,1);
-	len3 = FLAG[0];
-
-	count = ((len3 << 16) | (len2 << 8) | len1) & 0x07fffff;
-	cout<<"count ok"<< count <<endl;
-	//CMD[0]=BURST_FIFO_READ;
-	CMD[0]=SINGLE_FIFO_READ;
-
-	for (int i=0; i<count; i++)
-	{
-		lireSpi(0,CMD, FLAG,1);
-		image[i] = FLAG[0];
-		readbuf[i]=image[i];
-	}
-	cout<<endl;
-
-	close(spi_cs0_fd);
 }
