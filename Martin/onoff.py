4070007e684dd738b2b8fac72e9ce2ad22ca2d7d
Martin FOREST <97240787+mforest01@users.noreply.github.com>
2022-06-12 21:47:13 +0200

    Create onoff.py

---
+++ Martin/onoff.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import os
+import logging
+from time import sleep
+from pijuice import PiJuice
+
+pj = PiJuice(1,0x14)
+
+# Make sure wakeup_enabled and wakeup_on_charge have the correct values
+pj.rtcAlarm.SetWakeupEnabled(True)
+pj.power.SetWakeUpOnCharge(0)
+
+# Make sure power to the Raspberry Pi is stopped to not deplete
+# the battery
+pj.power.SetSystemPowerSwitch(0)
+pj.power.SetPowerOff(5)
+# Now turn off the system
+os.system("sudo shutdown -h now")
